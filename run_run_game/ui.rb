48cc4d6cb0e175d3fb532ae075baf4a7b12e9919
magro-ga <gabas9630@gmail.com>
2020-08-31 14:39:55 -0300

    atualização dos nomes e introdução de um novo jogo

---
+++ run_run_game/ui.rb
@@ -0,0 +1,8 @@
+def say_welcome
+    puts "Bem Vindo ao Foge-foge"
+    puts "Quall é o seu nome?"
+    name = gets.strip
+    puts "\n\n\n\n\n"
+    puts "Começaremos o jogo para Você, #{name}"
+    name
+end
